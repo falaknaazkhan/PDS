fe036f7b2bcbb680985c7c54defc7578ec24b205
falaknaazkhan <61285849+falaknaazkhan@users.noreply.github.com>
2025-05-10 01:33:20 +0100

    Update final_app_streamlit.py

--- final_app_streamlit.py
+++ final_app_streamlit.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Database and XML paths
-db_path = "final/final_oxfordshire.db"
+db_path = "final_oxfordshire.db"
 xml_path = "CouncilTaxData.xml"
 
 # Helper functions
